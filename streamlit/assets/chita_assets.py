3a33934e29e802af80b767edaddfd6cc11d819bd
awemipt <zadiran.nv@phystech.edu>
2024-10-31 21:38:24 +0300

    start developing logic of frontend

--- streamlit/assets/chita_assets.py
+++ streamlit/assets/chita_assets.py
@@ -1,9 +1,10 @@
 import streamlit as st
-
+from logic_pages import smc_input
 def chita_pages():
-    page = st.sidebar.radio("Страницы", ["Page 1", "Page 2", "Page 3"])
-    if page == "Page 1":
-        st.write("Эта страница может быть просмотрена только пользователем с ролью: Chita")
+    page = st.sidebar.radio("Страницы", ["SMC", "Page 2", "Page 3"])
+    if page == "SMC":
+       smc_input()
+            
     elif page == "Page 2":
         st.write("Эта страница может быть просмотрена только пользователем с ролью: Chita")
     elif page == "Page 3":
