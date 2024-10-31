d28e5b1bfb72e26f0748ec2dd4a0a68722939608
awemipt <zadiran.nv@phystech.edu>
2024-10-31 21:37:23 +0300

    fix

--- fastapi/server.py
+++ fastapi/server.py
@@ -1,9 +1,9 @@
-from routes import login
+from routes import login, smc
 from fastapi import FastAPI, File
 
 
 app = FastAPI(
-    title="Fall 2024 scince work N.V Zadiram",
+    title="Fall 2024 scince work N.V Zadiran",
     description="""Fullstack Fastapi Postgress Streamlit app for ores experimental data  Higland Gold
     """,
     version="0.1.0",
@@ -13,6 +13,7 @@ app = FastAPI(
 app.include_router(login.router, prefix="/login")
 
 
+app.include_router(smc.router, prefix="/smc")
 @app.get("/health_check")
 async def health_check():
     return {"status": "success"}
