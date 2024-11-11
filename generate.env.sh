c1651cc1112b67425d1e6b0ceeb3b45bf0940230
awemipt <zadiran.nv@phystech.edu>
2024-11-11 14:45:57 +0300

    env upd

--- generate.env.sh
+++ generate.env.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 GLOBAL_ENV_CONTENT="
+#ngninx config
+
+API_PORT=80
+FASTAPI_SERVER_ADDR=fastapi:8000
+STREAMLIT_FRONTEND_ADDR=streamlit:8501
 # =======================================================================================================================================================================================
 
-BACKEND_URL_DEV=http://localhost:8000
-# BACKEND_URL_DEV=http://host.docker.internal:8000
+# BACKEND_URL_DEV=http://localhost:8000
+BACKEND_URL_DEV=http://host.docker.internal:8000/api
 SECRETS_FILE_PATH=/fastapi/secrets/passwords.json
-STREAMLIT_FRONTED_ADDR=streamlit:8501
-FASTAPI_BACKEND_ADDR
 
 # =======================================================================================================================================================================================
 # Postgres setup
@@ -16,8 +19,13 @@ POSTGRES_HOST=db-postgres
 POSTGRES_PORT=5432
 POSTGRES_USER=postgres
 PGDATA=/var/lib/postgresql/data/pgdata
-POSTGRES_PASSWORD_FILE=/run/secrets/db-password
-DROP_TABLE_FLAG=True
+POSTGRES_PASSWORD=asdlksjdguihedyuigduifjkdfhufdrjkhffyhjsgdfyhujheujkygh2h34yh123
+DROP_TABLE_FLAG=False
+# =======================================================================================================================================================================================
+
+# FRONTEND CONFIG
+COOKIE_PASSWORD=fdjldsdsfusadhfuadshfaiudsgfqeuyrrbaekjlgblguwbgflewkbg
+COOKIE_PREFIX=SMC_DWT_TEST_COOKIE
 # =======================================================================================================================================================================================
 "
 
