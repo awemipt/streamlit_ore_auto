c50ada47835c4499b23453fb571d85f4f51a021c
--global <--global>
2024-11-22 10:01:55 +0300

    del old junk code

--- fastapi/crud/data_from_table.py
+++ fastapi/crud/data_from_table.py
@@ -4,42 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from models import DWT_RESULT       
 
-@dataclass
-class ore53:
-    
-    energy = np.array([0.4,0.25,0.1])
-    t10 = np.array([26.89, 17.99, 8.66])
 
-
-@dataclass
-class ore37_5:
-    
-    energy = np.array([1,0.25,0.1])
-    t10 = np.array([38.33, 13.74, 7.22])
-
-
-
-
-@dataclass
-class ore26_5:
-    
-    energy = np.array([2.5,1,0.25])
-    t10 = np.array([57.07, 32.28, 10.17])
-
-
-
-@dataclass
-class ore19:
-    
-    energy = np.array([2.49,1,0.25])
-    t10 = np.array([53.08, 27.46, 7.94])
-
-
-@dataclass
-class ore13_2:
-    
-    energy = np.array([2.5,1,0.25])
-    t10 = np.array([46.01, 20.74, 7.91])
 
 
 async def get_t10_data(db: AsyncSession, sample_name: str):
